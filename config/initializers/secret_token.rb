06761cd81ad8c285b1edd171be7bb5b55ee28007
Hector Bavio <hectorbavio@gmail.com>
2012-04-03 16:19:04 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudsponge::Application.config.secret_token = 'ae869d239bb5bfb356b4b01fcdeba092c67a21ce4bd10dd9c70429f02e60d12680010a769a52936dabdbd628b29751f143ccb22c513e7444478bd8a6f0a19cda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cloudsponge::Application.config.session_store :cookie_store, key: '_cloudsponge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cloudsponge::Application.config.session_store :active_record_store
